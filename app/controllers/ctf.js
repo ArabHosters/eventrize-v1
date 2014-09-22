d7e0d278c32a49b2cfc710df0ffd33adadfa3a40
Abdelrhman Talat <abdelrhman@arabhosters.com>
2014-09-22 17:43:48 +0300

    add 2 more windows and one lang

---
+++ app/controllers/ctf.js
@@ -0,0 +1,28 @@
+if (Alloy.Globals.currentLang == 'en') {
+	$.para.textAlign = 'left';
+	$.copyrights.textAlign = 'left';
+} else {
+	
+}
+$.para.text = L('ctf_para');
+$.eventrize.addEventListener("click",function(e){
+	Ti.Platform.openURL("http://eventrize.com");
+});
+
+
+$.swwebsite.addEventListener("click",function(e){
+	Ti.Platform.openURL("http://eventrize.com");
+});
+
+
+$.swfacebook.addEventListener("click",function(e){
+	Ti.Platform.openURL("https://www.facebook.com/eventrize");
+});	
+
+$.swtwitter.addEventListener("click",function(e){
+	Ti.Platform.openURL("https://twitter.com/Eventrize");
+});
+
+$.copyrights.addEventListener("click",function(e){
+	Ti.Platform.openURL("http://www.apache.org/licenses/LICENSE-2.0.html");
+});
